c7f900bc28dbbefa5d64b06b301d1e16711cc6ab
Douglas Santos <douglas.santos@materialize.pro>
2022-04-15 11:31:04 -0300

    Added Material imports, but they are disabled

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import { Elm } from "./Main.elm";
 
+// Material Design Imports 
+// require("material-components-web-elm/dist/material-components-web-elm.js");
+// require("material-components-web-elm/dist/material-components-web-elm.css");
+
 //Lazy
 const styles = import.meta.globEager("./Styles/**/*.scss");
 // import "./Styles/_index.scss";
